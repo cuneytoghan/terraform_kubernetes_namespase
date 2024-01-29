447245f73520e60d06fdc3a0884ba205fa3467df
Cuneyt Oghan <coghann@gmail.com>
2024-01-28 20:11:01 -0600

    add terraform files

---
+++ examples/main.tf
@@ -0,0 +1,4 @@
+module test-namespace {
+  source = "../"
+  name = "test-namespase"
+}
---
+++ variable.tf
@@ -0,0 +1,5 @@
+variable namespace {
+  type        = string
+  default     = ""
+  description = "please add your namespase name "
+}
